84298407f7d4c9375f83263e175ea0f25eca361c
AvissaN <38599693+AvissaN@users.noreply.github.com>
2021-05-15 13:58:57 -0400

    Initial commit

---
+++ 19-Week/19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,15 @@
+// Create a variable below
+var helloworld = "Hello this is my World!"; 
+
+// Console.log() your variable here.
+console.log(helloworld);
+
+var x =20;
+var y=10;
+var sum= x+y;
+console.log (sum);
+var menuItem = "Big Kahuna Burger" 
+var adj ="cute";
+var noun= "dog";
+var sentence = "The" +""+ adj +"" + noun +""+ "jumps in the river" ;
+console.log (sentence);
---
+++ 19-Week/19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,22 @@
+function addNumbers() {
+    var two = 2;
+    var three = 3;
+
+    var sum = two + three;
+
+    console.log(sum);    
+}
+
+addNumbers();
+
+// Create the stringConcat function here.
+function stringConcat() {
+    var taco = 'taco';
+    var cat = 'cat';
+
+    var tacoCat = taco + " " + cat;
+
+    alert(tacoCat);
+}
+
+stringConcat();xs
